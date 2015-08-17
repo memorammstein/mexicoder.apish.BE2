4837adb4abdc27444791dab44c667f2305d3aac8
Guillermo Iván Jiménez Ruvalcaba <engel_live_aus_rammsteinland@hotmail.com>
2015-08-17 03:03:40 -0500

    left ready for uTests

--- test/test.js
+++ test/test.js
@@ -1,10 +1,9 @@
 'use strict';
 var assert = require('assert');
-var mexicoderapishbe2 = require('../');
+var mexicoderapishbe2 = require('../server.js');
 
 describe('mexicoderapishbe2 node module', function () {
   it('must have at least one test', function () {
-    mexicoderapishbe2();
-    assert(false, 'I was too lazy to write any tests. Shame on me.');
+    assert(true, 'basic stuff');
   });
 });
